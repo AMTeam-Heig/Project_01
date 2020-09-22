1fb0a5a557b2640da2e970bd9eb19f852cb6d4e0
ClarFleu <clarisse.fleu@gmail.com>
2020-09-22 11:14:37 +0200

    Fix issue #2

--- src/main/java/ch/heigvd/amt/stackovergoat/services/QuestionManager.java
+++ src/main/java/ch/heigvd/amt/stackovergoat/services/QuestionManager.java
@@ -1,9 +1,6 @@
 package ch.heigvd.amt.stackovergoat.services;
 
 import ch.heigvd.amt.stackovergoat.model.Question;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.LinkedList;
 
@@ -13,22 +10,6 @@ public class QuestionManager {
         // TODO : get questions from file or database
         questions.add(new Question("What is GOAT?", 0));
         questions.add(new Question("Hello", 1));
-        return questions;
-    }
-
-    public LinkedList<Question> questionsFromJSON(String jsonFile) {
-        LinkedList<Question> questions = new LinkedList<>();
-
-        try {
-            JSONArray jsonArray = new JSONArray(new JSONObject(jsonFile).getJSONArray("questions"));
-
-            for (int i = 0; i < jsonArray.length(); ++i) {
-                JSONObject jsonQuestion = jsonArray.getJSONObject(i);
-                questions.add(new Question(jsonQuestion.getString("title"), jsonQuestion.getInt("id")));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
         return questions;
     }
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,7 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <head>
-    <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="../../css/stylesheet.css">
     <title>Welcome to StackOverGoat</title>
 </head>
 <html>
