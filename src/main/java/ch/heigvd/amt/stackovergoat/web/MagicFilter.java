00972cd971dc81b16d3af2bb719732e027edaaa9
ClarFleu <clarisse.fleu@gmail.com>
2020-09-25 08:56:22 +0200

    Add a magic filter

---
+++ src/main/java/ch/heigvd/amt/stackovergoat/web/MagicFilter.java
@@ -0,0 +1,23 @@
+package ch.heigvd.amt.stackovergoat.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+import static com.sun.activation.registries.LogSupport.log;
+
+@WebFilter(filterName = "MagicFilter")
+public class MagicFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        chain.doFilter(req, resp);
+        log("MagicFilter:doFilter()");
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+        log("MagicFilter:init()");
+    }
+
+}
