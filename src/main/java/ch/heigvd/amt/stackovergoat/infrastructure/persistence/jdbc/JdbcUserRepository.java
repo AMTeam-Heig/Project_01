4599b062e111dfd9c22dcd404d98118127a2daba
Massaoudi Walid <39918257+WalidMassaoudi@users.noreply.github.com>
2020-10-07 02:54:55 +0200

    JDBC structure start with user

--- src/main/java/ch/heigvd/amt/stackovergoat/infrastructure/persistence/jdbc/JdbcUserRepository.java
+++ src/main/java/ch/heigvd/amt/stackovergoat/infrastructure/persistence/jdbc/JdbcUserRepository.java
@@ -1,4 +1,41 @@
 package ch.heigvd.amt.stackovergoat.infrastructure.persistence.jdbc;
 
-public class JdbcUserRepository {
+import ch.heigvd.amt.stackovergoat.application.user.UsersQuery;
+import ch.heigvd.amt.stackovergoat.domain.user.IUserRepository;
+import ch.heigvd.amt.stackovergoat.domain.user.User;
+import ch.heigvd.amt.stackovergoat.domain.user.UserId;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class JdbcUserRepository implements IUserRepository {
+    @Override
+    public Collection<User> find(UsersQuery query) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public void remove(UserId id) {
+
+    }
+
+    @Override
+    public Optional<User> findById(UserId id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Collection<User> findAll() {
+        return null;
+    }
 }
