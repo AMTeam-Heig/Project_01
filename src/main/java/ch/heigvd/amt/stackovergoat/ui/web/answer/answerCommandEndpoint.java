05ca14c0fdc4a4e9f53584a94e0a0339eb7c121e
Massaoudi Walid <39918257+WalidMassaoudi@users.noreply.github.com>
2020-10-22 18:17:35 +0200

    beta version

---
+++ src/main/java/ch/heigvd/amt/stackovergoat/ui/web/answer/answerCommandEndpoint.java
@@ -0,0 +1,4 @@
+package ch.heigvd.amt.stackovergoat.ui.web.answer;
+
+public class answerCommandEndpoint {
+}
---
+++ src/main/java/ch/heigvd/amt/stackovergoat/ui/web/comment/commentCommandEndpoint.java
@@ -0,0 +1,4 @@
+package ch.heigvd.amt.stackovergoat.ui.web.comment;
+
+public class commentCommandEndpoint {
+}
