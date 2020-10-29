5ba77c23e2d40374c6f0785797a4bd4e54916cff
Massaoudi Walid <39918257+WalidMassaoudi@users.noreply.github.com>
2020-10-29 14:32:05 +0100

    we run all in one

---
+++ docker/images/build-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#make sure that we generate the WAR
+# mvn  -f ../../../pom.xml clean package
+#build the image by the Dockerfile
+cp ../../target/Project_01.war ./openliberty/liberty/artifact/Project_01.war
+docker build -t stackover-goat/stackovergoat .
